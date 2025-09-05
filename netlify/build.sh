a0cf92332ea816b40d86384b6b2aee3001dbb23a
Abner Bogan <abnerbog@buffalo.edu>
2025-09-05 17:16:58 -0400

    Use other instructions for micromamba installation

--- netlify/build.sh
+++ netlify/build.sh
@@ -6,15 +6,16 @@ set -e
 # git submodule init
 # git submodule update --remote
 
-# install micromamba
-curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj -C /tmp
-ls -l /tmp
-/tmp/bin/micromamba shell init -p ~/micromamba 
-eval "$(/tmp/bin/micromamba shell hook -s posix)"
+# Download and extract Micromamba
+wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+# Set up environment variables
+export MAMBA_ROOT_PREFIX="$PWD/micromamba"
+export PATH="$PWD/bin:$PATH"
 
 # create and activate environment
-/tmp/bin/micromamba env create -f environment.yml
-/tmp/bin/micromamba activate water-content-portal-env
+micromamba env create -f environment.yml
+micromamba activate water-content-portal-env
 
 # build documentation
 mkdocs build
