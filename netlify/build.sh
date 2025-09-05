f78593f0a1af66538ff01ef287e5aa67fd21f03c
Abner Bogan <abnerbog@buffalo.edu>
2025-09-05 17:22:35 -0400

    Add git submodules related commands

--- netlify/build.sh
+++ netlify/build.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# # update submodules
-# git submodule init
-# git submodule update --remote
-
 # Download and extract Micromamba
 wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 
@@ -15,7 +11,10 @@ export PATH="$PWD/bin:$PATH"
 
 # create and activate environment
 micromamba env create -f environment.yml
-# micromamba activate water-content-portal-env
+
+# update submodules
+git submodule init
+git submodule update --remote
 
 # build documentation
 micromamba run -n water-content-portal-env mkdocs build
