6148f491e1605eb8ee87e6630d4600cf26ca1091
Abner Bogan <abnerbog@buffalo.edu>
2025-09-05 16:52:08 -0400

    Update build.sh for updating content and building environment + docs

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+set -e
+
+# update submodules
 git submodule init
 git submodule update --remote
+
+# install micromamba
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj -C /tmp
+/tmp/micromamba shell init -p ~/micromamba
+eval "$(/tmp/micromamba shell hook -s posix)"
+
+# create and activate environment
+/tmp/micromamba env create -f environment.yml
+/tmp/micromamba activate water-content-portal-env
+
+# build documentation
 mkdocs build
-# mkdocs build -f mkdocs-minify.yml
