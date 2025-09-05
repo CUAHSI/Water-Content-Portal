28a616c0f26138509021909f061f4d1aea04abd3
Abner Bogan <abnerbog@buffalo.edu>
2025-09-05 17:11:17 -0400

    Change path to micromamba install

--- netlify/build.sh
+++ netlify/build.sh
@@ -9,12 +9,12 @@ set -e
 # install micromamba
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj -C /tmp
 ls -l /tmp
-/tmp/micromamba shell init -p ~/micromamba 
-eval "$(/tmp/micromamba shell hook -s posix)"
+/tmp/bin/micromamba shell init -p ~/micromamba 
+eval "$(/tmp/bin/micromamba shell hook -s posix)"
 
 # create and activate environment
-/tmp/micromamba env create -f environment.yml
-/tmp/micromamba activate water-content-portal-env
+/tmp/bin/micromamba env create -f environment.yml
+/tmp/bin/micromamba activate water-content-portal-env
 
 # build documentation
 mkdocs build
