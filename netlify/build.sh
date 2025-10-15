7645528ab7649bddfd72fb805d6bd584403a79fc
Abner Bogan <abnerbog@buffalo.edu>
2025-10-15 12:56:09 -0400

    Modify netlify build to ingest/update submodules

--- netlify/build.sh
+++ netlify/build.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-
 set -e
-
-# Download and extract Micromamba
-wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-
-# Set up environment variables
-export MAMBA_ROOT_PREFIX="$PWD/micromamba"
-export PATH="$PWD/bin:$PATH"
-
-# create and activate environment
-micromamba env create -f environment.yml
 
 # update submodules
 git submodule init
 git submodule update --remote
 
 # build documentation
-micromamba run -n water-content-portal-env mkdocs build
+make ci
